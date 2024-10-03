74bc0150c434c648d40ec0c1fcd411b136158f94
Nigel Andrews <nigel.andrews@epita.fr>
2024-10-03 18:29:19 +0200

    feat: basic scene class

---
+++ src/scene/scene.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+#include <memory>
+
+#include "object/object.h"
+
+namespace yart
+{
+    class scene
+    {
+    public:
+        void add_object(object* object)
+        { objects_.emplace_back(object); }
+
+        void add_object(std::unique_ptr<object> object)
+        {
+            objects_.push_back(std::move(object));
+        }
+
+    private:
+        std::vector<std::unique_ptr<object>> objects_;
+    };
+} // namespace yart
