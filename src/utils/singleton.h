6887c479e82642ace18d41f64966f4e915952abb
Nigel Andrews <nigel.andrews@epita.fr>
2024-10-04 18:35:23 +0200

    feat: singleton template

---
+++ src/utils/singleton.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "traits.h"
+
+namespace yart::utils
+{
+    template <typename T>
+    class singleton
+        : non_copyable
+        , non_movable
+    {
+    protected:
+        static T& get_instance()
+        {
+            static T instance;
+            return instance;
+        }
+
+        singleton() = default;
+        ~singleton() = default;
+    };
+} // namespace yart::utils
