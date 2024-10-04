5db4dd52bb2a3079bdb348e78df0f4d88344d446
Nigel Andrews <nigel.andrews@epita.fr>
2024-10-04 17:15:47 +0200

    feat: camera class

---
+++ src/scene/camera.cpp
@@ -0,0 +1,24 @@
+#include "camera.h"
+
+#include <glm/geometric.hpp>
+#include <glm/gtx/string_cast.hpp>
+#include <iostream>
+
+namespace yart
+{
+    void camera::look_at(const glm::vec3& position, const glm::vec3& up, const glm::vec3& target)
+    {
+        position_ = position;
+        target_ = target;
+
+        const auto view_direction = glm::normalize(target - position);
+        right_ = glm::normalize(glm::cross(glm::normalize(up), view_direction));
+        up_ = glm::normalize(glm::cross(view_direction, right_));
+    }
+
+    glm::vec3 camera::compute_ray_direction(const glm::vec2& screen_pos) const
+    {
+        // Finding the point on the screen
+        return glm::normalize(glm::vec3{ 0.f, 0.f, -near_plane_ } + right_ * screen_pos.x + up_ * screen_pos.y);
+    }
+}// namespace yart
---
+++ src/scene/camera.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <memory>
+
+
+namespace yart
+{
+    // Camera space is normalized to [-1, 1].
+    // Here, it is the renderer's job to abstract the screen size rendered and the camera only has to handle the normalized space
+    class camera
+    {
+    public:
+        constexpr camera(float near_plane = 1.f, float fov = 90.f)
+        : near_plane_(near_plane)
+        , fov_(fov)
+        {}
+
+        void look_at(const glm::vec3& position, const glm::vec3& up, const glm::vec3& target);
+
+        // Screen must be normalized
+        glm::vec3 compute_ray_direction(const glm::vec2& screen_pos) const;
+
+        template <class Self>
+        auto&& position_get(this Self&& self)
+        {
+            return std::forward<Self>(self).position_;
+        }
+
+    private:
+        glm::vec3 position_;
+        glm::vec3 target_;
+        glm::vec3 up_;
+        glm::vec3 right_;
+        float near_plane_;
+        float fov_;
+    };
+} // namespace yart
